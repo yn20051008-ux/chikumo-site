3910347ec879bd90ae128264bdf6183718099d10
yn20051008-ux <yn20051008@gmail.com>
2025-12-22 23:39:38 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  const root = document.querySelector('[data-surface="cosmos-viewport"]');
-  if (!root) return;
-
-  window.addEventListener('load', () => {
-    root.classList.add('is-awakened');
-  });
-})();
